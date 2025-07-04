7b27f898e60843e6bf257710cdae34d87ecd082e
alireza <alireza@nativeblocks.io>
2025-07-04 11:07:01 +0400

    support Parse from dsl stringify

--- nbx.go
+++ nbx.go
@@ -1,8 +1,12 @@
 package nbx
 
 import (
+	"errors"
 	"github.com/nativeblocks/nbx/internal/compiler"
+	"github.com/nativeblocks/nbx/internal/lexer"
 	"github.com/nativeblocks/nbx/internal/model"
+	"github.com/nativeblocks/nbx/internal/parser"
+	"strings"
 )
 
 type FrameJson = model.FrameJson
@@ -17,4 +21,16 @@ func ToDSL(frame FrameJson) FrameDSLModel {
 // ToJSON converts a FrameDSLModel to a FrameJson, validating with the given schema and frameID.
 func ToJSON(frameDSL FrameDSLModel, schema string, frameID string) (FrameJson, error) {
 	return compiler.ToJson(frameDSL, schema, frameID)
+}
+
+// Parse parses a stringify DSL and returns a FrameDSLModel.
+// It returns an error if parsing fails, including parser errors joined by semicolons.
+func Parse(stringifyDsl string) (FrameDSLModel, error) {
+	l := lexer.NewLexer(stringifyDsl)
+	p := parser.NewParser(l)
+	frame := p.ParseSDUI()
+	if frame == nil {
+		return FrameDSLModel{}, errors.New(strings.Join(p.Errors(), "; "))
+	}
+	return *frame, nil
 }
